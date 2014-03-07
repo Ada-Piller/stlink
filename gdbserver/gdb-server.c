42fabaa2275fb42283ecd4dae6998e75951b6324
Michael Roessler <michael.roessler@piller.com>
2014-03-07 13:55:50 +0100

    Target description F4 expand (two memory regions added, FSMC registers and FSMC bank 1)

--- gdbserver/gdb-server.c
+++ gdbserver/gdb-server.c
@@ -315,6 +315,8 @@ static const char* const memory_map_template_F4 =
   "    <property name=\"blocksize\">0x20000</property>"						//128kB
   "  </memory>"
   "  <memory type=\"ram\" start=\"0x40000000\" length=\"0x1fffffff\"/>" 	// peripheral regs
+  "  <memory type=\"ram\" start=\"0xA0000000\" length=\"0xFFF\"/>"		 	// FSMC regs
+  "  <memory type=\"ram\" start=\"0x60000000\" length=\"0xFFFFFFF\"/>"		// FSMC bank 1
   "  <memory type=\"ram\" start=\"0xe0000000\" length=\"0x1fffffff\"/>" 	// cortex regs
   "  <memory type=\"rom\" start=\"0x1fff0000\" length=\"0x7800\"/>"         // bootrom
   "  <memory type=\"rom\" start=\"0x1fffc000\" length=\"0x10\"/>"        	// option byte area
